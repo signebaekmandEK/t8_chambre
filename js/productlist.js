21de9fb30d44f482a225b0cbbc67b7ad4e0b9b1b
Signe <siba1000@stud.ek.dk>
2026-03-18 23:13:40 +0100

    footerogproductlistret

--- js/productlist.js
+++ js/productlist.js
@@ -6,11 +6,20 @@ const listURL = category ? `https://dummyjson.com/products/category/${category}`
 const listContainer = document.querySelector(".productlist-container");
 document.querySelector("h2").textContent = category ? category : "Products";
 
+let allProducts = [];
+// SortBy buttons
+const sortPriceBtnLh = document.querySelector("#sortPriceBtnLh");
+const sortPriceBtnHl = document.querySelector("#sortPriceBtnHl");
+// filterbtn
+const discountBtn = document.getElementById("filterDiscountBtn");
+let discountActive = false;
+
 function getProducts() {
   fetch(listURL)
     .then((res) => res.json())
     .then((data) => {
-      showProducts(data.products);
+      allProducts = data.products;
+      showProducts(allProducts);
     })
     .catch((err) => console.error("Error fetching products:", err));
 }
@@ -25,19 +34,40 @@ function showProducts(products) {
                 <div class="product-indhold">
                     <h3>${product.brand}</h3>
                     <p class="price">Price: ${product.price} DKK</p>
-                    <p class="size">Size: N/A</p>
-                    <p class="color">...</p>
+                    <p class="color">Color:</p>
+                    <img class="farvevalg" src="SVG/farvevalg.svg" alt="Eksisterende farver du kan vælge imellem til din Bruno BookCase">
+                     <p class="size">Size: N/A</p>
                 </div>
                 <br>
                 <a class="productdetailsB" href="productdetails.html?id=${product.id}">Buy</a>
                   </article>`;
   });
 }
+// Price Low to High
+function sortByPriceAsc() {
+  const sorted = [...allProducts].sort((a, b) => a.price - b.price);
+  showProducts(sorted);
+}
+sortPriceBtnLh.addEventListener("click", sortByPriceAsc);
+
+function sortByPriceDesc() {
+  const sorted = [...allProducts].sort((a, b) => b.price - a.price);
+  showProducts(sorted);
+}
+
+sortPriceBtnHl.addEventListener("click", sortByPriceDesc);
+
+// filter
+discountBtn.addEventListener("click", () => {
+  discountActive = !discountActive;
+  discountBtn.classList.toggle("active");
+
+  let filteredProducts = allProducts;
+  if (discountActive) {
+    filteredProducts = allProducts.filter((p) => p.discountPercentage);
+  }
+
+  showProducts(filteredProducts);
+});
 
 getProducts();
-
-/* filtrerting og sorting knapper 
-
-/* const sortPriceBtnLh = document.querySelector("#sortPriceBtnLh");
-const sortPriceBtnHl = document.querySelector("#sortPriceBtnHl");
-const filterWomenBtn = document.querySelector("#filterWomenBtn"); */
